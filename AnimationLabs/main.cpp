7dfc9c81fbd14f40127b69f0055b0456ca1a1529
Berta Backmann <ylva.f@gmx.at>
2017-01-20 12:04:24 +0000

    Add project files.

---
+++ AnimationLabs/main.cpp
@@ -0,0 +1,202 @@
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include <string> 
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+// Macro for indexing vertex buffer
+#define BUFFER_OFFSET(i) ((char *)NULL + (i))
+
+using namespace std;
+
+bool LoadFile(const std::string& fileName, std::string& outShader)
+{
+	std::ifstream file(fileName);
+	if(!file.is_open())
+	{
+		std::cout << "Error Loading file: " << fileName << " - impossible to open file" << std::endl;
+		return false;
+	}
+
+	if(file.fail())
+	{
+		std::cout << "Error Loading file: " << fileName << std::endl;
+		return false;
+	}
+
+	std::stringstream stream;
+	stream << file.rdbuf();
+	file.close();
+
+	outShader = stream.str();
+
+	return true;
+}
+
+void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+{
+	// create a shader object
+	GLuint ShaderObj = glCreateShader(ShaderType);
+
+	if (ShaderObj == 0) {
+		fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+		exit(0);
+	}
+	// Bind the source code to the shader, this happens before compilation
+	glShaderSource(ShaderObj, 1, (const GLchar**)&pShaderText, NULL);
+	// compile the shader and check for errors
+	glCompileShader(ShaderObj);
+	GLint success;
+	// check for shader related errors using glGetShaderiv
+	glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		GLchar InfoLog[1024];
+		glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+		fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+		exit(1);
+	}
+	// Attach the compiled shader object to the program object
+	glAttachShader(ShaderProgram, ShaderObj);
+}
+
+GLuint CompileShaders(const std::string& vsFilename, const std::string& psFilename)
+{
+	//Start the process of setting up our shaders by creating a program ID
+	//Note: we will link all the shaders together into this ID
+	GLuint shaderProgramID = glCreateProgram();
+	if (shaderProgramID == 0) {
+		fprintf(stderr, "Error creating shader program\n");
+		exit(1);
+	}
+
+	// Create two shader objects, one for the vertex, and one for the fragment shader
+	std::string vs, ps;
+	LoadFile(vsFilename, vs);
+	AddShader(shaderProgramID, vs.c_str(), GL_VERTEX_SHADER);
+	LoadFile(psFilename, ps);
+	AddShader(shaderProgramID, ps.c_str(), GL_FRAGMENT_SHADER);
+
+	GLint Success = 0;
+	GLchar ErrorLog[1024] = { 0 };
+
+	// After compiling all shader objects and attaching them to the program, we can finally link it
+	glLinkProgram(shaderProgramID);
+	// check for program related errors using glGetProgramiv
+	glGetProgramiv(shaderProgramID, GL_LINK_STATUS, &Success);
+	if (Success == 0) {
+		glGetProgramInfoLog(shaderProgramID, sizeof(ErrorLog), NULL, ErrorLog);
+		fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+		exit(1);
+	}
+
+	// program has been successfully linked but needs to be validated to check whether the program can execute given the current pipeline state
+	glValidateProgram(shaderProgramID);
+	// check for program related errors using glGetProgramiv
+	glGetProgramiv(shaderProgramID, GL_VALIDATE_STATUS, &Success);
+	if (!Success) {
+		glGetProgramInfoLog(shaderProgramID, sizeof(ErrorLog), NULL, ErrorLog);
+		fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+		exit(1);
+	}
+	// Finally, use the linked shader program
+	// Note: this program will stay in effect for all draw calls until you replace it with another or explicitly disable its use
+	glUseProgram(shaderProgramID);
+	return shaderProgramID;
+}
+
+GLuint generateObjectBuffer(GLfloat vertices[], GLfloat colors[]) {
+	GLuint numVertices = 3;
+	// Genderate 1 generic buffer object, called VBO
+	GLuint VBO;
+ 	glGenBuffers(1, &VBO);
+	// In OpenGL, we bind (make active) the handle to a target name and then execute commands on that target
+	// Buffer will contain an array of vertices 
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// After binding, we now fill our object with data, everything in "Vertices" goes to the GPU
+	glBufferData(GL_ARRAY_BUFFER, numVertices*7*sizeof(GLfloat), NULL, GL_STATIC_DRAW);
+	// if you have more data besides vertices (e.g., vertex colours or normals), use glBufferSubData to tell the buffer when the vertices array ends and when the colors start
+	glBufferSubData (GL_ARRAY_BUFFER, 0, numVertices*3*sizeof(GLfloat), vertices);
+	glBufferSubData (GL_ARRAY_BUFFER, numVertices*3*sizeof(GLfloat), numVertices*4*sizeof(GLfloat), colors);
+return VBO;
+}
+
+void linkCurrentBuffertoShader(GLuint shaderProgramID){
+	GLuint numVertices = 3;
+	// find the location of the variables that we will be using in the shader program
+	GLuint positionID = glGetAttribLocation(shaderProgramID, "vPosition");
+	GLuint colorID = glGetAttribLocation(shaderProgramID, "vColor");
+	// Have to enable this
+	glEnableVertexAttribArray(positionID);
+	// Tell it where to find the position data in the currently active buffer (at index positionID)
+    glVertexAttribPointer(positionID, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	// Similarly, for the color data.
+	glEnableVertexAttribArray(colorID);
+	glVertexAttribPointer(colorID, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(numVertices*3*sizeof(GLfloat)));
+}
+
+void display(){
+
+	glClear(GL_COLOR_BUFFER_BIT);
+	// NB: Make the call to draw the geometry in the currently activated vertex buffer. This is where the GPU starts to work!
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+    glutSwapBuffers();
+}
+
+void init()
+{
+	// Create 3 vertices that make up a triangle that fits on the viewport 
+	GLfloat vertices[] = {-1.0f, -1.0f, 0.0f,
+			1.0f, -1.0f, 0.0f,
+			0.0f, 1.0f, 0.0f};
+	// Create a color array that identfies the colors of each vertex (format R, G, B, A)
+	GLfloat colors[] = {0.0f, 1.0f, 0.0f, 1.0f,
+			1.0f, 0.0f, 0.0f, 1.0f,
+			0.0f, 0.0f, 1.0f, 1.0f};
+	
+	// Set up the shaders
+	GLuint shaderProgramID = CompileShaders("Shaders/diffuse.vs", "Shaders/diffuse.ps");
+	
+	// Put the vertices and colors into a vertex buffer object
+	generateObjectBuffer(vertices, colors);
+
+	// Link the current buffer to the shader
+	linkCurrentBuffertoShader(shaderProgramID);	
+}
+
+int main(int argc, char** argv){
+
+	// Set up the window
+	glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Hello Triangle");
+	// Tell glut where the display function is
+	glutDisplayFunc(display);
+
+	 // A call to glewInit() must be done after glut is initialized!
+    GLenum res = glewInit();
+	// Check for any errors
+    if (res != GLEW_OK) {
+      fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+      return 1;
+    }
+	// Set up your objects and shaders
+	init();
+	// Begin infinite event loop
+	glutMainLoop();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
